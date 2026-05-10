4c092c23eff5e2a297202e1d7382f943f140745a
João Pedro Cabral Machado <joao.cabral@autojun.com.br>
2026-05-09 23:51:43 -0300

    Versão Headless do Site

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/studyflow-landing-page/',
   server: {
     host: "::",
     port: 8080,
